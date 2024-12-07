60e3839cc1ace6eeae175562fde1c70192c8e710
wylie-allister <000840304@mohawkcollege.ca>
2024-12-07 12:20:03 -0500

    Assign balls to variables

--- wylie-allister-a5-project/Scripts/BbBall.cs
+++ wylie-allister-a5-project/Scripts/BbBall.cs
@@ -6,7 +6,6 @@ public partial class BbBall : RigidBody2D
 	[Export]
 	int ballValue = 1;
 
-	int[] batBall = { 1, 2, 3, 4, 5 };
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -15,10 +14,24 @@ public partial class BbBall : RigidBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-        int randomBat = batBall[GD.Randi() % batBall.Length];
+        RigidBody2D bbBall = GetNode<RigidBody2D>("BB Ball");
+		RigidBody2D hwBall = GetNode<RigidBody2D>("HW Ball");
+		RigidBody2D lbbBall = GetNode<RigidBody2D>("LBB Ball");
+		RigidBody2D erbBall = GetNode<RigidBody2D>("ERB Ball");
+		RigidBody2D gfbBall = GetNode<RigidBody2D>("GFB Ball");
+		RigidBody2D vbBall = GetNode<RigidBody2D>("VB Ball");
+		RigidBody2D hhBall = GetNode<RigidBody2D>("HH Ball");
+		RigidBody2D snbBall = GetNode<RigidBody2D>("SNB Ball");
+		RigidBody2D bebBall = GetNode<RigidBody2D>("BEB Ball");
+		RigidBody2D lnbBall = GetNode<RigidBody2D>("LNB Ball");
+		RigidBody2D ffbBall = GetNode<RigidBody2D>("FFB Ball");
+
+        int randomBat = GD.RandRange(0, 5);
         if (this.ballValue == randomBat)
 		{
 
+			
+			
 		}
 	}
 }

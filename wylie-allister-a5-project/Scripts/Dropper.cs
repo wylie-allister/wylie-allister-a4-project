d38955fbac308ba79755124995b8147f8e6bc177
wylie-allister <000840304@mohawkcollege.ca>
2024-12-07 11:29:08 -0500

    Add collision to balls

--- wylie-allister-a5-project/Scripts/Dropper.cs
+++ wylie-allister-a5-project/Scripts/Dropper.cs
@@ -5,9 +5,16 @@ public partial class Dropper : Node2D
 {
 	[Export]
 	float speed = 300;
+
+	[Export]
+	private PackedScene Prefab;
+
+	[Export]
+	private Node2D BallParent;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,5 +22,13 @@ public partial class Dropper : Node2D
 	{
 		float moveX = Input.GetAxis("left", "right") * speed * (float)delta;
 		Translate(new Vector2 (moveX, 0));
+		if (Input.IsActionJustPressed("drop"))
+		{
+			
+		}
+	}
+	public void GetBat(BbBall ball)
+	{
+		
 	}
 }
